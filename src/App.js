b5a7523af5fbd9968a0d5390636192862f10a93c
673800357 <673800357@qq.com>
2018-07-06 19:59:46 +0800

    路由

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import Login from "./containers/login/Login";
+import Main from "./containers/main/Main";
 import { BrowserRouter,Route } from 'react-router-dom';
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
       <div>
-       <Route exact path="/"></Route>
-       <Route path="/about" component={()=><div>123</div>} />
+       <Route exact path="/" component={Main}/>
+       <Route path="/login" component={Login} />
        </div>
       </BrowserRouter>
     );
