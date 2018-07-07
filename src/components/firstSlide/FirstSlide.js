3b6e9b34432a1435780012bd3887645a6139bdd7
qiuruolin <qiu.ruolin@sumixer.com>
2018-07-07 19:28:58 +0800

    侧栏布局修改

--- src/components/firstSlide/FirstSlide.js
+++ src/components/firstSlide/FirstSlide.js
@@ -31,75 +31,38 @@ class FirstSlide extends React.Component {
                     ]
                 }
             ],
-            startDir: [
-                {
-                    id: "shdjd",
-                    value: "大三第一学期",
-                    childrens: [
-                        {
-                            id: "sjdkd",
-                            value: "计算机网络"
-                        }
-                    ]
-                },
-                {
-                    id: "kkaaa",
-                    value: "大三第二学期",
-                    childrens: [
-                        {
-                            id: "sjdkd",
-                            value: "操作系统"
-                        },
-                        {
-                            id: "ss",
-                            value: "概率论"
-                        }
-                    ]
-                }
-            ],
             showClass: true,
-            showStar: false,
             showCChild: [],
-            showSChild: []
+            curParentIndex: 0,
+            curIndex: 0
         }
     }
     componentDidMount() {
-        const {classDir, startDir} = this.state;
+        const {classDir} = this.state;
         let initC = [];
-        let initS = [];
         for(let i = 0; i < classDir.length; i++){
             initC.push(true)
         }
-        for(let i = 0; i < startDir.length; i++){
-            initS.push(false)
-        }
         this.setState({
-            showCChild: initC,
-            showSChild: initS
+            showCChild: initC
         })
     }
-    toggleShow(index, type) {
-        console.log(index);
-        const {showCChild, showSChild} = this.state;
-        let arr = [];
-        if(type === "class"){
-            arr = showCChild;
-            arr[index] = !showCChild[index];
-            this.setState({
-                showCChild: arr
-            })
-        }
-        else{
-            arr = showSChild;
-            arr[index] = !showSChild[index];
-            this.setState({
-                showSChild: arr
-            })
-        }
-
+    toggleShow(index) {
+        const {showCChild} = this.state;
+        let arr = showCChild;
+        arr[index] = !showCChild[index];
+        this.setState({
+            showCChild: arr
+        })
+    }
+    selectItem(i, index){
+        this.setState({
+            curIndex: i,
+            curParentIndex: index
+        })
     }
     render(){
-        const {classDir, startDir, showClass, showStar, showCChild, showSChild} = this.state;
+        const {classDir, showClass, showCChild, curIndex, curParentIndex} = this.state;
         return(
             <div className="first-slide">
                 <div className="new-note">
@@ -117,41 +80,15 @@ class FirstSlide extends React.Component {
                             classDir.map((item, index) => (
                                 <div className={showClass? "show": "hide"} key={index}>
                                     <div className="slide-item sub-item">
-                                        <i className={["iconfont show-icon", showCChild[index]? "icon-shouqi": "icon-zhankai"].join(" ")} onClick={this.toggleShow.bind(this, index, "class")}></i>
+                                        <i className={["iconfont show-icon", showCChild[index]? "icon-shouqi": "icon-zhankai"].join(" ")} onClick={this.toggleShow.bind(this, index)}></i>
                                         <i className="iconfont icon-wenjianjia show-icon item-icon"></i>
                                         <span className="item-title">{item.value}</span>
                                     </div>
                                     {
                                         item.childrens.map((data, i) => (
-                                            <div className={["slide-item final-item", showCChild[index]? "show": "hide"].join(" ")} key={i}>
+                                            <div className={["slide-item final-item", showCChild[index]? "show": "hide"].join(" ")} onClick={this.selectItem.bind(this, i, index)} key={i}>
                                                 <i className="iconfont icon-wenben show-icon item-icon"></i>
-                                                <span className="item-title">{data.value}</span>
-                                            </div>
-                                        ))
-                                    }
-                                </div>
-                            ))
-                        }
-                    </div>
-                    <div className="class-note">
-                        <div className="slide-item">
-                            <i className={["iconfont show-icon", showStar? "icon-shouqi": "icon-zhankai"].join(" ")} onClick={() => this.setState({showStar: !showStar})}></i>
-                            <i className="iconfont icon-star show-icon item-icon"></i>
-                            <span className="item-title">重点笔记</span>
-                        </div>
-                        {
-                            startDir.map((item, index) => (
-                                <div className={showStar? "show": "hide"} key={index}>
-                                    <div className="slide-item sub-item">
-                                        <i className={["iconfont show-icon", showSChild[index]? "icon-shouqi": "icon-zhankai"].join(" ")} onClick={this.toggleShow.bind(this, index, "star")}></i>
-                                        <i className="iconfont icon-wenjianjia show-icon item-icon"></i>
-                                        <span className="item-title">{item.value}</span>
-                                    </div>
-                                    {
-                                        item.childrens.map((data, i) => (
-                                            <div className={["slide-item final-item", showSChild[index]? "show": "hide"].join(" ")} key={i}>
-                                                <i className="iconfont icon-wenben show-icon item-icon"></i>
-                                                <span className="item-title">{data.value}</span>
+                                                <span className={["item-title", curIndex === i && curParentIndex === index? "active" : ""].join(" ")}>{data.value}</span>
                                             </div>
                                         ))
                                     }
