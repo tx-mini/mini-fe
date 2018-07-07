82128ab0978fc36b70d30955af2a7a0c3ea8e528
qiuruolin <qiu.ruolin@sumixer.com>
2018-07-07 15:32:41 +0800

    添加头部相关布局及样式

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import './header.css'
+import logo from '../../assets/img/logo.png'
 
 const Header = () =>{
     return (
         <div className="header-bar">
-            header
+            <div className="logo">
+                <img src={logo} className="logo-img" alt="logo"/>
+                <div className="logo-title">企鹅笔记</div>
+            </div>
+            <div className="logo">
+                <div className="logo-title">我是用户名</div>
+                <div className="logo-title exit">退出</div>
+            </div>
         </div>
     )
 }
