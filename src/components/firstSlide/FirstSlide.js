eebe305f68ccf1b2d95ec0565dd538493ad3b72f
qiuruolin <qiu.ruolin@sumixer.com>
2018-07-09 20:29:43 +0800

    提取数据至父组件

--- src/components/firstSlide/FirstSlide.js
+++ src/components/firstSlide/FirstSlide.js
@@ -5,37 +5,17 @@ class FirstSlide extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            classDir: [
-                {
-                    id: "shdjd",
-                    value: "大三第一学期",
-                    childrens: [
-                        {
-                            id: "sjdkd",
-                            value: "计算机网络"
-                        }
-                    ]
-                },
-                {
-                    id: "kkaaa",
-                    value: "大三第二学期",
-                    childrens: [
-                        {
-                            id: "sjdkd",
-                            value: "操作系统"
-                        },
-                        {
-                            id: "ss",
-                            value: "概率论"
-                        }
-                    ]
-                }
-            ],
+            classDir: props.classDir,
             showClass: true,
             showCChild: [],
             curParentIndex: 0,
             curIndex: 0
         }
+    }
+    componentWillReceiveProps(nextProps) {
+        this.setState({
+            classDir: nextProps.classDir
+        })
     }
     componentDidMount() {
         const {classDir} = this.state;
@@ -55,38 +35,44 @@ class FirstSlide extends React.Component {
             showCChild: arr
         })
     }
-    selectItem(i, index){
+    selectItem(i, index, data){
         this.setState({
             curIndex: i,
             curParentIndex: index
         })
+        //传递选中的data
+        this.props.SelectItem(data);
+    }
+    createNote(){
+        //新建笔记
+        this.props.createNote();
     }
     render(){
         const {classDir, showClass, showCChild, curIndex, curParentIndex} = this.state;
         return(
             <div className="first-slide">
-                <div className="new-note">
+                <div className="new-note" onClick={this.createNote.bind(this)}>
                     <i className="iconfont icon-add add-icon"></i>
                     新建笔记
                 </div>
                 <div className="slide-content">
                     <div className="class-note">
-                        <div className="slide-item">
-                            <i className={["iconfont show-icon", showClass? "icon-shouqi": "icon-zhankai"].join(" ")} onClick={() => this.setState({showClass: !showClass})}></i>
+                        <div className="slide-item" onClick={() => this.setState({showClass: !showClass})}>
+                            <i className={["iconfont show-icon", showClass? "icon-shouqi": "icon-zhankai"].join(" ")} ></i>
                             <i className="iconfont icon-iconset0117 show-icon item-icon"></i>
                             <span className="item-title">课堂笔记</span>
                         </div>
                         {
                             classDir.map((item, index) => (
                                 <div className={showClass? "show": "hide"} key={index}>
-                                    <div className="slide-item sub-item">
-                                        <i className={["iconfont show-icon", showCChild[index]? "icon-shouqi": "icon-zhankai"].join(" ")} onClick={this.toggleShow.bind(this, index)}></i>
+                                    <div className="slide-item sub-item" onClick={this.toggleShow.bind(this, index)}>
+                                        <i className={["iconfont show-icon", showCChild[index]? "icon-shouqi": "icon-zhankai"].join(" ")} ></i>
                                         <i className="iconfont icon-wenjianjia show-icon item-icon"></i>
                                         <span className="item-title">{item.value}</span>
                                     </div>
                                     {
                                         item.childrens.map((data, i) => (
-                                            <div className={["slide-item final-item", showCChild[index]? "show": "hide"].join(" ")} onClick={this.selectItem.bind(this, i, index)} key={i}>
+                                            <div className={["slide-item final-item", showCChild[index]? "show": "hide"].join(" ")} onClick={this.selectItem.bind(this, i, index, data)} key={i}>
                                                 <i className="iconfont icon-wenben show-icon item-icon"></i>
                                                 <span className={["item-title", curIndex === i && curParentIndex === index? "active" : ""].join(" ")}>{data.value}</span>
                                             </div>
--- src/containers/main/Main.js
+++ src/containers/main/Main.js
@@ -3,12 +3,58 @@ import Header from '../../components/header/Header'
 import FirstSlide from '../../components/firstSlide/FirstSlide'
 import OperationArea from '../../components/operationArea/OperationArea'
 export default class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      classDir: [
+        {
+            id: "shdjd",
+            value: "大三第一学期",
+            childrens: [
+                {
+                    id: "sjdkd",
+                    value: "计算机网络"
+                }
+            ]
+        },
+        {
+            id: "kkaaa",
+            value: "大三第二学期",
+            childrens: [
+                {
+                    id: "sjdkd",
+                    value: "操作系统"
+                },
+                {
+                    id: "ss",
+                    value: "概率论"
+                }
+            ]
+        }
+      ],
+      classList: [
+        { name: "7.7计算机网络", time: Date.now(), id: 0 },
+        { name: "7.9计算机网络", time: Date.now(), id: 1 }
+      ],
+      category: "计算机网络"
+    };
+  }
+  componentDidMount() {
+    //获取目录数据
+  }
+  SelectItem(data){
+    console.log(data);
+  }
+  createNote(){
+    console.log("新建")
+  }
   render() {
+    const {classDir, category, classList} = this.state;
     return (
       <div className="main">
         <Header/>
-        <FirstSlide/>
-        <OperationArea/>
+        <FirstSlide classDir={classDir} SelectItem={this.SelectItem.bind(this)} createNote={this.createNote.bind(this)}/>
+        <OperationArea category={category} classList={classList}/>
       </div>
     )
   }
